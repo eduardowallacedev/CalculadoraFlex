d383df1989fb50d9ce20cecef33c32cc8ccebc85
Logon Aluno <logonaluno@local.com>
2019-12-14 12:08:44 -0300

    Adicionado comentário

--- app/src/main/java/com/eduardo/calculadoraflex/MyApplication.kt
+++ app/src/main/java/com/eduardo/calculadoraflex/MyApplication.kt
@@ -6,7 +6,11 @@ import com.facebook.stetho.Stetho
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        // Bloqueia alteração do user_preference somente para modo DEBUG do app
         if (BuildConfig.DEBUG)
+        //
+
             Stetho.initializeWithDefaults(this)
     }
 }
