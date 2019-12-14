45d638248db5e120455e0d8ad9c2ab4065f0b1dc
Logon Aluno <logonaluno@local.com>
2019-12-14 15:30:43 -0300

    Incluído activity para tratamento do login

--- app/src/main/java/com/eduardo/calculadoraflex/ui/login/LoginActivity.kt
+++ app/src/main/java/com/eduardo/calculadoraflex/ui/login/LoginActivity.kt
@@ -3,18 +3,43 @@ package com.eduardo.calculadoraflex.ui.login
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.eduardo.calculadoraflex.R
+import com.eduardo.calculadoraflex.ui.form.FormActivity
 import com.eduardo.calculadoraflex.ui.signup.SignUpActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     val NEW_USER_REQUEST = 1
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        if (mAuth.currentUser != null) {
+            goToHome()
+        }
+
+        btLogin.setOnClickListener {
+            mAuth.signInWithEmailAndPassword(
+                inputLoginEmail.text.toString(),
+                inputLoginPassword.text.toString()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    goToHome()
+                } else {
+                    Toast.makeText(this@LoginActivity, it.exception?.message,
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
         btSignup.setOnClickListener {
             val criarConta =
@@ -24,20 +49,26 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    private fun goToHome() {
+        val intent = Intent(this, FormActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        finish()
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
+        when(requestCode) {
             NEW_USER_REQUEST -> {
-                when (resultCode) {
+                when(resultCode) {
                     Activity.RESULT_OK -> {
                         inputLoginEmail
                             .setText(data?.getStringExtra("email"))
                     }
                 }
             }
-            else -> {
-            }
+            else -> {}
         }
-
     }
 }
+
