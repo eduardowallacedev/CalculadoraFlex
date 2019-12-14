5f6df025d109cbcb6ad8a4067425d0fa3f00c296
Logon Aluno <logonaluno@local.com>
2019-12-14 14:49:38 -0300

    alterado de IF para WHEN

--- app/src/main/java/com/eduardo/calculadoraflex/ui/login/LoginActivity.kt
+++ app/src/main/java/com/eduardo/calculadoraflex/ui/login/LoginActivity.kt
@@ -1,13 +1,36 @@
 package com.eduardo.calculadoraflex.ui.login
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.eduardo.calculadoraflex.R
+import com.eduardo.calculadoraflex.ui.signup.SignUpActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val NEW_USER_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btSignup.setOnClickListener {
+            val criarConta =
+                Intent(this, SignUpActivity::class.java)
+
+            startActivityForResult(criarConta, NEW_USER_REQUEST)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // when no lugar do if
+        when(requestCode){
+            NEW_USER_REQUEST -> {}
+            else -> {}
+        }
+
     }
 }
